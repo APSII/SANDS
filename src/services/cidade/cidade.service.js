8eb8f85bdaf2222f064c2789e8d1fe0835d8f1a8
Guilherme Mossi <guilhermeissom@outlook.com>
2020-03-03 19:00:29 -0300

    Tirar paginação cidade

--- src/services/cidade/cidade.service.js
+++ src/services/cidade/cidade.service.js
@@ -1,19 +1,19 @@
 // Initializes the `cidade` service on path `/cidade`
-const { Cidade } = require('./cidade.class');
-const createModel = require('../../models/cidade.model');
-const hooks = require('./cidade.hooks');
+const { Cidade } = require("./cidade.class");
+const createModel = require("../../models/cidade.model");
+const hooks = require("./cidade.hooks");
 
-module.exports = function (app) {
+module.exports = function(app) {
   const options = {
     Model: createModel(app),
-    paginate: app.get('paginate')
+    paginate: false
   };
 
   // Initialize our service with any options it requires
-  app.use('/cidade', new Cidade(options, app));
+  app.use("/cidade", new Cidade(options, app));
 
   // Get our initialized service so that we can register hooks
-  const service = app.service('cidade');
+  const service = app.service("cidade");
 
   service.hooks(hooks);
 };
